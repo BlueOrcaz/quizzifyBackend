961ab11527c8c3962042456a9ce3587cede8ae5d
BlueOrcaz <bondoldman@gmail.com>
2024-02-18 18:29:04 +1000

    Initial Commit

---
+++ src/main/java/dev/BlueOrcaz/Quizzify/QuizzifyApplication.java
@@ -0,0 +1,13 @@
+package dev.BlueOrcaz.Quizzify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzifyApplication.class, args);
+	}
+
+}
